7803a7368341b43fe947298fe04d7a1953720c64
Tomasz Zebrun <tomasz.zebrun@tron.net.pl>
2021-06-10 04:46:20 +0200

    6.2 First test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,25 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        Calculator calculator = new Calculator();
+
+        int resultAdd = calculator.add(3, 6);
+        int resultSubtract = calculator.subtract(6,2);
+
+        if (resultAdd == 9) {
+            System.out.println("test add OK");
+        } else {
+            System.out.println("Error add!");
+        }
+
+        if (resultSubtract == 4) {
+            System.out.println("test subtract OK");
+        } else {
+            System.out.println("Error subtract!");
+        }
     }
 }
