bee00391d4d32c5a945245e63019afb9a81bc25c
Tomasz Zebrun <tomasz.zebrun@tron.net.pl>
2021-12-12 17:05:30 +0100

    31.1 Search Laptop on eBay

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/config/WebDriverConfig.java
@@ -0,0 +1,16 @@
+package com.kodilla.testing2.config;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverConfig {
+    public final static String CHROME = "CHROME_DRIVER";
+    public static WebDriver getDriver(final String driver) {
+        System.setProperty("webdriver.chrome.driver", "/Users/tomaszzebrun/IdeaProjects/kodilla-course/chromedriver");
+        if (driver.equals(CHROME)) {
+            return new ChromeDriver();
+        } else {
+            return null;
+        }
+    }
+}
